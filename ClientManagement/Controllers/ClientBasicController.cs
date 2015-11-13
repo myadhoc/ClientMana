5cf0269eb5a502b118acd7c2d2fce854a74ac1c7
myadhoc <myadhoc@gmail.com>
2015-11-13 13:11:40 +0800

    客戶後台管理

---
+++ ClientManagement/Controllers/ClientBasicController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ClientManagement.Models;
+
+namespace ClientManagement.Controllers
+{
+    public class ClientBasicController : Controller
+    {
+        private 客戶資料Entities db = new 客戶資料Entities();
+
+        // GET: /ClientBasic/     
+        public ActionResult Index(string keyword)
+        {
+            if (keyword != null)
+            {
+                return View(db.客戶資料.Where(c => c.是否已刪除 == false).Where(c => c.客戶名稱.Contains(keyword)).ToList());
+            }
+            else
+            {
+                return View(db.客戶資料.Where(c => c.是否已刪除 == false).ToList());
+            }
+
+        }
+
+        // GET: /ClientBasic/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            客戶資料 客戶資料 = db.客戶資料.Find(id);
+            if (客戶資料 == null)
+            {
+                return HttpNotFound();
+            }
+            return View(客戶資料);
+        }
+
+        // GET: /ClientBasic/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /ClientBasic/Create
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="Id,客戶名稱,統一編號,電話,傳真,地址,Email,是否已刪除")] 客戶資料 客戶資料)
+        {
+            if (ModelState.IsValid)
+            {
+                db.客戶資料.Add(客戶資料);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(客戶資料);
+        }
+
+        // GET: /ClientBasic/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            客戶資料 客戶資料 = db.客戶資料.Find(id);
+            if (客戶資料 == null)
+            {
+                return HttpNotFound();
+            }
+            return View(客戶資料);
+        }
+
+        // POST: /ClientBasic/Edit/5
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="Id,客戶名稱,統一編號,電話,傳真,地址,Email,是否已刪除")] 客戶資料 客戶資料)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(客戶資料).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(客戶資料);
+        }
+
+        // GET: /ClientBasic/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            客戶資料 客戶資料 = db.客戶資料.Find(id);
+            if (客戶資料 == null)
+            {
+                return HttpNotFound();
+            }
+            return View(客戶資料);
+        }
+
+        // POST: /ClientBasic/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            客戶資料 客戶資料 = db.客戶資料.Find(id);
+
+            if (客戶資料 != null)
+            {
+                客戶資料.是否已刪除 = true;
+                //db.客戶資料.Remove(客戶資料);
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ClientManagement/Models/Validation/MobilePhoneValidate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace ClientManagement.Models.Validation
+{
+    public class MobilePhoneValidate : ValidationAttribute
+    {
+      
+        
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value != null)
+            {
+                if (Regex.IsMatch(value.ToString(), "\\d{4}-\\d{6}") == true)
+                {
+                    return ValidationResult.Success;
+                }
+                else
+                {
+                    var errorMsg = string.Format("手機格式必須是 09xx-xxxxxx!");
+
+                    return new ValidationResult(errorMsg);
+
+                }
+            }
+            else
+            {
+                return ValidationResult.Success;
+            }
+         
+        }
+    }
+}
